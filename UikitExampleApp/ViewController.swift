a217712ac4777289e218a46b1569c278306ac5f0
Ilona Shcherbakova <shcherbakova0911@mail.ru>
2019-09-27 16:22:52 +0300

    Initial Commit

---
+++ UikitExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UikitExampleApp
+//
+//  Created by Ilona Shcherbakova on 27/09/2019.
+//  Copyright © 2019 Ilona Shcherbakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
